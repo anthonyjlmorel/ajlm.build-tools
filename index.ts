cc95a572a0cec1fc7bc4cf491f81efb63b1a3bf3
Anthony <anthony.jlmorel@gmail.com>
2019-09-18 15:52:36 +0200

    Starting Implementing command calling on each node

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@ import { resolve, join, dirname } from "path";
 import { ParametersGetter, TCommandLineParameters } from 'ajlm.utils';
 import { TreeExecutor } from './tree-executor';
 
-import { exec } from "child_process";
-
 // module entry point
 
 
@@ -24,25 +22,7 @@ if(param["exec"] && param["target"]){
     rsr.getPackageCompilationSpec(resolve(pck))
     .then((specs)=>{
 
-        (new TreeExecutor()).execute(specs, async (node: TSpec)=>{
-            return new Promise<void>((resolve, reject)=>{
-
-                var child = exec(action, {
-                    cwd: dirname(node.path)
-                });
-                child.stdout.on('data', function(data) {
-                    console.log('stdout: ' + data);
-                });
-                child.stderr.on('data', function(data) {
-                    console.log('stderr: ' + data);
-                });
-                child.on('close', function(code) {
-                    // Depending on code ... reject or resolve and display info
-                    console.log('closing code: ' + code);
-                    resolve();
-                });
-            });
-        });
+        (new TreeExecutor()).executeCommand(specs, action);
 
     });
 
--- tree-executor.ts
+++ tree-executor.ts
@@ -1,16 +1,50 @@
 import { TSpec } from 'repository-specs-reader';
 import { MapBasedDepthFirstSearch, TreeTraversalType } from 'ajlm.utils';
+import { exec } from 'child_process';
+import { dirname } from 'path';
 
 export class TreeExecutor {
 
-    public async execute( rootNode: TSpec, callback: (node: TSpec) => Promise<void> ): Promise<void>{
+    private readonly adjacentNodeReader: (node: TSpec)=> Promise<TSpec[]> = async (node: TSpec)=>{
+        return Object.keys(node.dependencies).map(d => node.dependencies[d]);
+    };
 
-        let dfs = new MapBasedDepthFirstSearch( async (node: TSpec)=>{
-            return Object.keys(node.dependencies).map(d => node.dependencies[d]);
-        }, async (node: TSpec)=>{ return node.name; } );
+    private readonly nodeHash: (node: TSpec) => Promise<string> = async (node: TSpec)=>{ return node.name; };
 
+    /**
+     * Executes an action on each node following a DFS algorithm
+     */
+    public async executeAction( rootNode: TSpec, callback: (node: TSpec) => Promise<void> ): Promise<void>{
 
+        let dfs = new MapBasedDepthFirstSearch( this.adjacentNodeReader, this.nodeHash );
         await dfs.perform(rootNode, callback, TreeTraversalType.PostOrder);
     }
 
+    /**
+     * Executes a command line in each package folder following a DFS algorithm
+     */
+    public async executeCommand(rootNode: TSpec, command: string): Promise<void> {
+        
+        // @TODO improve logging ...
+        
+        return this.executeAction(rootNode, async (node: TSpec)=>{
+            return new Promise<void>((resolve, reject)=>{
+
+                var child = exec(command, {
+                    cwd: dirname(node.path)
+                });
+                child.stdout.on('data', function(data) {
+                    console.log('stdout: ' + data);
+                });
+                child.stderr.on('data', function(data) {
+                    console.log('stderr: ' + data);
+                });
+                child.on('close', function(code) {
+                    // Depending on code ... reject or resolve and display info
+                    console.log('closing code: ' + code);
+                    resolve();
+                });
+            });
+        });
+    }
 }
